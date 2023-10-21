3b46bd3875b94d190c01b4d1989cc219823cd379
m-bilal-331 <rao.mbilal331@gmail.com>
2023-10-21 14:54:58 +0500

    Power Calculator

---
+++ Solutions/s02.js
@@ -0,0 +1,9 @@
+
+//https://edabit.com/challenge/wAdE9te55cowBLcPs
+
+function circuitPower(voltage, current) {
+	return voltage*current;
+}
+
+const power = circuitPower(230,10);
+console.log(power);
