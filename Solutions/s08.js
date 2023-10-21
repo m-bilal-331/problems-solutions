4e88993d1697736f2c82f0fa7b0c19ccb0953535
m-bilal-331 <rao.mbilal331@gmail.com>
2023-10-21 15:49:07 +0500

    Is the Number Less than or Equal to Zero?

---
+++ Solutions/s08.js
@@ -0,0 +1,12 @@
+//https://edabit.com/challenge/PTiLYyb4A69KZtBCg
+
+
+function lessThanOrEqualToZero(num) {
+	if (num <= 0)
+		return true;
+	else
+		return false;
+	
+}
+
+console.log(lessThanOrEqualToZero(5));
