6e7c6d7b5e83391a13be84cdd92b8c3aaea6570f
m-bilal-331 <rao.mbilal331@gmail.com>
2023-10-21 15:44:00 +0500

    Find the Perimeter of a Rectangle

---
+++ Solutions/s07.js
@@ -0,0 +1,9 @@
+//https://edabit.com/challenge/XnJ24rWW7iJkNrtsh
+
+function findPerimeter(length, width) {
+	return ((length+width)*2)
+
+}
+
+const perimeter = findPerimeter(6,7);
+console.log(perimeter);
