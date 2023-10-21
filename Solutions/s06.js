457ddc83999a8b34567ccbbee8fcabce0736007c
m-bilal-331 <rao.mbilal331@gmail.com>
2023-10-21 15:40:52 +0500

    Return Something to Me!

---
+++ Solutions/s06.js
@@ -0,0 +1,10 @@
+//https://edabit.com/challenge/MvZK536X7fyrWH8Qc
+
+
+function giveMeSomething(a) {
+	let string1 = "something ";
+	return string1.concat(a);
+}
+
+let string = giveMeSomething("is  better than nothing");
+console.log(string);
