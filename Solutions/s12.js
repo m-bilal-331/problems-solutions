8fad68eeff586cd38ae0cae533301fb2cef40432
m-bilal-331 <rao.mbilal331@gmail.com>
2023-10-22 15:51:58 +0500

    Left Shift by Powers of Two

---
+++ Solutions/s12.js
@@ -0,0 +1,10 @@
+//https://edabit.com/challenge/pB6CF3rFBi8ykJ3Br
+
+
+function shiftToLeft(x, y) {
+	let result;
+	result = (x * (Math.pow(2,y)));
+	return result;
+}
+
+console.log(shiftToLeft(-32,2));
