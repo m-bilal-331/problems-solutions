f85c52d294ae696791f6384b90c9c8e1345cc586
m-bilal-331 <rao.mbilal331@gmail.com>
2023-10-23 14:12:05 +0500

    Find the Smallest and Biggest Numbers

---
+++ Solutions/s13.js
@@ -0,0 +1,18 @@
+//
+
+function minMax(arr) {
+    if (arr.length === 0) {
+      return [undefined, undefined];
+    }
+  
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+  
+    return [min, max];
+  }
+  
+  // Example usage:
+  const arr = [12,74,929,3,0,83];
+  const [min, max] = minMax(arr);
+  console.log([min,max]);
+  
